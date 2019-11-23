cdae484e93f943281b7048682a0214bc180fe0ed
Hugo Fabricio <me@hugofabricio.com>
2019-11-23 16:56:05 -0300

    Finished github repository list app.

---
+++ src/routes.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import Main from './pages/Main';
+import Repository from './pages/Repository';
+
+const Routes = () => (
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" exact component={Main} />
+      <Route path="/repository/:repository" component={Repository} />
+    </Switch>
+  </BrowserRouter>
+);
+
+export default Routes;
